e4082889ff723bd2bdc9d128711ab25aeaef560c
Zobee <zohebboga@hotmail.com>
2021-06-15 16:03:17 -0400

    Implemented integration test for appointment deletion

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -32,33 +32,61 @@ describe("Application", () => {
 
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
     const {container, debug} = render(<Application />)
+    //Wait for initial get from API to render
     await waitForElement(() => getByText(container, "Archie Cohen"))
-    //console.log(prettyDOM(container))
     const appointments = getAllByTestId(container, "appointment")
-    //console.log(prettyDOM(appointments))
     const appointment = appointments[0]
-    //console.log(prettyDOM(appointment))
+    //Add a new appointment by clicking 'add', inputting the student name, and selecting an interviewer
     fireEvent.click(getByAltText(appointment, "Add"));
-    //console.log("AFTER CLICK EVENT")
-    //console.log(prettyDOM(appointment))
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" }
     })
-    // console.log("AFTER INPUT")
-    // console.log(prettyDOM(appointment))
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"))
     fireEvent.click(getByText(appointment, "Save"))
-    // console.log("AFTER SAVE")
-    // console.log(prettyDOM(appointment));
-    //debug(appointment)
+    //Check to confirm that the Status component is mounted with a message of 'saving'
     expect(getByText(appointment, "Saving")).toBeInTheDocument()
 
+    //wait for the status component to be unmounted, and replaced with the Show component
     await waitForElementToBeRemoved(() => getByText(appointment, "Saving"))
     expect(getByText(appointment, "Lydia Miller-Jones"))
-    //debug(appointment)
+
+    //check to make sure the number of appointments is incremented, and that 'no spots remaining' is shown
     const monday = getAllByTestId(container, "day")
     .find(day => queryByText(day, "Monday"))
-    //debug(monday)
     expect(getByText(monday, /no spots remaining/i)).toBeInTheDocument()
+  })
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    const {container} = render(<Application />)
+    await waitForElement(() => getByText(container, "Archie Cohen"))
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+    fireEvent.click(getByAltText(appointment,'Delete'))
+    expect(getByText(appointment, /are you sure/i)).toBeInTheDocument()
+    
+    fireEvent.click(getByText(appointment, "Confirm"))
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument()
+
+    await waitForElementToBeRemoved(() => getByText(appointment, "Deleting"))
+    expect(getByAltText(appointment, "Add"))
+
+    const monday = getAllByTestId(container, "day")
+    .find(day => queryByText(day, "Monday"))
+    expect(getByText(monday, /2 spots remaining/i)).toBeInTheDocument()
+  })
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    const {container, debug} = render(<Application />)
+    await waitForElement(() => getByText(container, "Archie Cohen"))
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+    fireEvent.click(getByAltText(appointment, "Edit"))
+    debug(appointment)
+  })
+  xit("shows the save error when failing to save an appointment", async () => {
+
+  })
+  xit("shows the delete error when failing to delete an existing appointment", async () => {
+
   })
 })
