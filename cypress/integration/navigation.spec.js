4197ff6ab3715f47dad5042feeb09adda89e56be
Zobee <zohebboga@hotmail.com>
2021-06-16 15:03:58 -0400

    Implemented basic navigation test with Cypress

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,14 @@
 describe("Navigation", () => {
+  forEvery()
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    cy
+    .contains("[data-testid=day]", "Tuesday")
+    .click()
+    .should("have.class", "day-list__item--selected");
+  })
 });
