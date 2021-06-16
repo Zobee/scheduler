fc24c0a87bab76fb9470b93cad38ce1902c08d91
Zobee <zohebboga@hotmail.com>
2021-06-16 15:21:58 -0400

    Implemented test for booking a new appointment

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,13 +1,23 @@
-describe("Interacting With Appointments", () => {
+describe("Appointments", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset")
+    cy.visit("/")
+    cy.contains("Monday");
+  })
   it("should book an interview", () => {
-    /*
-    Visits the root of our web server
-    Clicks on the "Add" button in the second appointment
-    Enters their name
-    Chooses an interviewer
-    Clicks the save button
-    Sees the booked appointment
-    */
+    // Clicks on the "Add" button in the second appointment
+    cy.get("[alt=Add]")
+    .first()
+    .click()
+    // Enters their name
+    cy.get('[data-testid=student-name-input]').type("Lydia Miller-Jones")
+    // Chooses an interviewer
+    cy.get("[alt='Sylvia Palmer']").click()
+    // Clicks the save button
+    cy.contains("Save").click()
+    // Sees the booked appointment
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
   })
 
   it("should edit an interview", () => {
