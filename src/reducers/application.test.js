62f4da9d73815d0ae26e87452402f282442bbe0e
Zobee <zohebboga@hotmail.com>
2021-06-16 17:40:28 -0400

    updated config.yml to include a deploy job

--- src/reducers/application.test.js
+++ src/reducers/application.test.js
@@ -1,6 +1,9 @@
 import reducer from './application';
 
-it("throws an error with an unsupported type", () => {
-  expect(() => reducer({},{type:"banana"}))
-  .toThrowError("Tried to reduce with unsupported action type: banana")
+describe("Application", () => {
+  
+  it("throws an error with an unsupported type", () => {
+    expect(() => reducer({},{type:"banana"}))
+    .toThrowError("Tried to reduce with unsupported action type: banana")
+  })
 })
