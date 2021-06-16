083cb7412a002e5227848430973a2d3e9bf1321d
Zobee <zohebboga@hotmail.com>
2021-06-16 15:33:26 -0400

    Implemented test for editing an existing appointment

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -21,21 +21,21 @@ describe("Appointments", () => {
   })
 
   it("should edit an interview", () => {
-    /*
-    Visits the root of our web server
-    Clicks the edit button for the existing appointment
-    Changes the name and interviewer
-    Clicks the save button
-    Sees the edit to the appointment
-    */
+    cy.get("[alt=Edit]")
+    .click({force: true})
+    cy.get('[data-testid=student-name-input]').clear().type("Frank Frankson")
+    cy.get("[alt='Tori Malcolm']").click()
+    cy.contains("Save").click()
+    cy.contains(".appointment__card--show", "Frank Frankson");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   })
 
-  it("should cancel an interview", () => {
-    /*
-    Visits the root of our web server
-    Clicks the delete button for the existing appointment
-    Clicks the confirm button
-    Sees that the appointment slot is empty
-    */
-  })
+//   it("should cancel an interview", () => {
+//     /*
+//     Visits the root of our web server
+//     Clicks the delete button for the existing appointment
+//     Clicks the confirm button
+//     Sees that the appointment slot is empty
+//     */
+//   })
 })
